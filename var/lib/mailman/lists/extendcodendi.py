c587301392c26f5211e04420c6d26826785b37ae
Codendi <codendi-xerox@lists.partners.xrce.xerox.com>
2011-05-31 17:45:06 +0200

    Don't hard code db parameters in extend.py

--- var/lib/mailman/lists/extendcodendi.py
+++ var/lib/mailman/lists/extendcodendi.py
@@ -1,14 +1,54 @@
 from Mailman.MySQLConnector import MySQLConnector
 from Mailman.ForgeSecurityManager import ForgeSecurityManager
 import os
+import re
+
+##############################
+# Local Configuration Load
+# Taken from Codendi/src/utils/include.py
+##############################
+def load_local_config(filename):
+    """Local Configuration Load"""
+    try:
+        f = open(filename)
+    except IOError, (errno, strerror):
+        print "Can't open %s: I/O error(%s): %s" % (filename, errno, strerror)
+    else:
+        comment_pat   = re.compile("^\s*\/\/")
+        empty_pat     = re.compile("^\s*$")
+        assign_pat    = re.compile("^\s*\$(.*);\s*$")
+        nodollar_pat  = re.compile("(\s+)\$")
+        dottoplus_pat = re.compile("(\s+)\.(\s+)")
+        # filter end of line comment, but beware of things like http:// or ldap:// -> make sure there is a blank char before the '//'
+        nocomment_pat = re.compile(" \/\/.*")
+        while True:
+            line = f.readline()
+            if not line: break
+            if comment_pat.match(line) or empty_pat.match(line): continue
+            m = assign_pat.match(line)
+            if m is not None:
+                n = nodollar_pat.sub(" ",m.group(1))
+                n = dottoplus_pat.sub(" + ",n)
+                n = nocomment_pat.sub("",n)
+                exec n in globals()
+        f.close()
+
+def load_codendi_conf():
+    db_include = os.getenv('CODENDI_LOCAL_INC','')
+    if db_include is "":
+        db_include = "/etc/codendi/conf/local.inc"
+    load_local_config(db_include)
+    load_local_config(db_config_file)
+    
 
 def extendMemberAdaptor(list):
+    load_codendi_conf()
     dbparam={}
     #Config to connect to database
-    dbparam['dbhost'] = "127.0.0.1"
-    dbparam['dbuser']= "codendiadm"
-    dbparam['dbpassword'] = "mypassword"
-    dbparam['database'] = "codendi"
+    dbparam['dbhost'] = sys_dbhost
+    dbparam['dbuser']= sys_dbuser
+    dbparam['dbpassword'] = sys_dbpasswd
+    dbparam['database'] = sys_dbname
     dbparam['refresh'] = 360
 
     dbparam['mailman_table']= 'plugin_mailman'#table where mailman stores memeberships info
